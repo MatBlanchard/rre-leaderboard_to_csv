f14713551b4e3590f5fa2c2524e3af8a1c898b13
MatBlanchard <matblanchard.pro@gmail.com>
2023-03-19 22:58:43 +0100

    Correct some bugs

--- raceroom.py
+++ raceroom.py
@@ -3,6 +3,7 @@ import requests
 import csv
 import json
 import configparser
+from exceptions import TooManyErrorsException
 
 config = configparser.RawConfigParser()
 config.read('raceroom.ini', encoding='utf-8')
@@ -12,8 +13,9 @@ save_directory = config.get('RRE', 'save_directory')
 car_ids = ast.literal_eval(config.get('RRE', 'car_id_list'))
 driver_name = config.get('RRE', 'player')
 header = ast.literal_eval(config.get('RRE', 'header'))
-finished_classes = ['GTR 3', 'Porsche 911 GT3 R (2019)', 'BMW M6 GT3', 'Ferrari 488 GT3 EVO 2020']
+finished_classes = []
 count = 1500
+max_errors = 10
 
 
 def get_json_file():
@@ -23,39 +25,47 @@ def get_json_file():
 
 def get_lap_time_sec(lap_time):
     if len(lap_time) == 2:
-        return '{:.3f}'.format(float(lap_time[0])*60 + float(lap_time[1])).replace('.', ',')
+        return '{:.3f}'.format(float(lap_time[0]) * 60 + float(lap_time[1])).replace('.', ',')
     else:
         return '{:.3f}'.format(float(lap_time[0])).replace('.', ',')
 
 
-def get_data(track_id, car_id, car_name):
+def get_data(track_id, car_id):
+    wr, lap_time, rank, i = None, None, None, 0
+    errors = 0
     while True:
+        if errors >= max_errors:
+            raise TooManyErrorsException
         url = "https://game.raceroom.com/leaderboard/listing/0?start=0&count=" + \
-                  str(count) + "&track=" + str(track_id) + "&car_class=" + str(car_id)
+              str(count) + "&track=" + str(track_id) + "&car_class=" + str(car_id)
         page = requests.get(url, headers={"X-Requested-With": "XMLHttpRequest"})
+        if not page.ok:
+            errors += 1
+            continue
         file = json.loads(page.text)
         context = file['context']['c']['results']
         if len(context) == 0:
-            if car_name in finished_classes and track_id != 10274:
+            if car_id in finished_classes and track_id != 10274:
+                errors += 1
                 continue
             else:
                 return []
         wr = context[0]['laptime']
         wr = wr.split('s')[0].split('m ')
         wr = get_lap_time_sec(wr)
-        lap_time = ""
-        i = 0
-        rank = ""
         for c in context:
             i += 1
             if c['driver']['name'] == driver_name:
                 lap_time = c['laptime'].split('s')[0].split('m ')
                 lap_time = get_lap_time_sec(lap_time)
                 rank = i
-        if car_name in finished_classes:
-            if not wr or not lap_time or not rank:
+        if car_id in finished_classes:
+            if not wr or not lap_time or not rank or not i:
+                errors += 1
                 continue
         break
+    if not wr or not lap_time or not rank or not i:
+        return []
     return [wr, lap_time, rank, i]
 
 
@@ -66,11 +76,10 @@ def save_data(json_file, car_id):
         writer.writerow(header)
         n = 1
         for t in tracks:
-            data = [n] + [t[0]] + get_data(t[1], car_id, car_name)
+            data = [n] + [t[0]] + get_data(t[1], car_id)
             if len(data) == len(header):
                 writer.writerow(data)
-                string = "Car: " + car_name + " | Track: " + t[0] + " saved successfully"
-                print(string)
+                print("Car: " + car_name + " | Track: " + t[0] + " saved successfully")
                 n += 1
 
 
@@ -103,3 +112,6 @@ if __name__ == "__main__":
         print("\nProgram interrupted\n")
     except PermissionError as e:
         print("\nError: " + e.filename + " already opened, please close it and retry\n")
+    except TooManyErrorsException:
+        print("\nError: exceeding max number of try ("
+              + str(max_errors) + " tries max) while getting the data\n")
